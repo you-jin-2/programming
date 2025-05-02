d82c3bf83c24846bd31a9ed775509f5abbe6e733
you-jin-2 <dldbwlswls0002@gmail.com>
2025-05-02 11:32:07 +0900

    git test

---
+++ src/etc/git_test2.java
@@ -0,0 +1,40 @@
+package etc;
+
+import java.util.Iterator;
+
+public class git_test2 implements Runnable {
+
+	
+	static int a = 0; 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    git_test2 runnable = new git_test2();
+	    Thread thread = new Thread(runnable);
+
+	    //start() 메서드를 이용하여 Thread를 실행
+	    thread.start();
+	     try {
+	    	 a++;thread.run();	
+	    	 a++;thread.run();
+	    	 a++;thread.run();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	}
+	
+    @Override
+    public synchronized void  run() {
+        // 쓰레드가 실행할 작업 정의
+    	
+    	System.out.println();
+    	for (int i = 0; i < 100; i++) {
+    		System.out.print(a);
+		}
+    }
+    
+}
+
+ 
